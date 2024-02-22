694fe299997489a4312969192c134c7cc48fd49b
Shewanji <marrionwanjiru26@gmail.com>
2024-02-22 20:14:40 +0000

    fix pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """module that uses in-place"""
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
     """
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
