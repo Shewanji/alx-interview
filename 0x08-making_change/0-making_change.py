a044faba2e4599cc2dc86ac488061e225e70eb90
Shewanji <marrionwanjiru26@gmail.com>
2024-02-29 21:48:42 +0000

    improve efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,15 +10,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    max_value = total + 1
-    dp = [max_value] * (total + 1)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for j in range(coin, total + 1):
             dp[j] = min(dp[j], dp[j - coin] + 1)
 
-    if dp[total] > total:
+    if dp[total] == float('inf'):
         return -1
     else:
         return dp[total]
